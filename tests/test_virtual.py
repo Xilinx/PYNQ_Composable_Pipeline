b890d6adfed769cd86684a41a5edf9917fd8a100
Mario Ruiz <mruiznog@xilinx.com>
2022-05-06 11:39:00 +0100

    Add intial pytest for the virtual.py file

---
+++ tests/test_virtual.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2022 Xilinx, Inc
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from pynq_composable import virtual
+
+__author__ = "Mario Ruiz"
+__copyright__ = "Copyright 2022, Xilinx"
+__email__ = "pynq_support@xilinx.com"
+
+
+def test_streaming_ip():
+    name = 'highpass_fir'
+    ip = virtual.StreamingIP(name)
+    assert ip._fullpath == name
+
+
+def test_unloaded_ip():
+    name = 'filter2d'
+    ip = virtual.UnloadedIP(name)
+    assert ip._fullpath == name
+
+
+def test_buffer_ip():
+    name = 'filter2d'
+    ip = virtual.BufferIP(name)
+    assert ip._fullpath == name
