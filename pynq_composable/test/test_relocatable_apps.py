b6df570f7430ab43556f39f7a8cf71128d89e5db
Mario Ruiz <mruiznog@amd.com>
2022-10-17 08:33:11 +0100

    Full test to verify all possible app combinations

--- pynq_composable/test/test_relocatable_apps.py
+++ pynq_composable/test/test_relocatable_apps.py
@@ -2,25 +2,12 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from pynq_composable import Composable, VideoStream, VSource, VSink
+from pynq_composable import VideoStream, VSource, VSink
 import pytest
 from pynq import Overlay
 import time
 import itertools
 
-
-"""
-
-1. Define list of partial bitstreams per app
-2. Create permutation list
-3. Download partial bitstreams
-4. iterate over the pipeline to get the actual object
-5. compose
-6. start video
-7. Wait for 5 secs
-8.
-
-"""
 
 app_construct = [
     (['subtract', 'filter2d'],
@@ -35,6 +22,14 @@ app_construct = [
     (['cornerharris', 'add'], ['ps_video_in', 'duplicate_accel',
      [['rgb2gray_accel', 'pr/cornerHarris', 'gray2rgb_accel'], [1]],
      'pr/add_accel', 'ps_video_out']),
+
+    (['dilate', 'dilate', 'bitand'], ['ps_video_in', 'duplicate_accel',
+     [['rgb2gray_accel', 'colorthresholding_accel', 'pr/erode', 'pr/dilate',
+     'pr/dilate', 'pr/erode'], [1]], 'pr/bitwise', 'ps_video_out']),
+
+    (['add'], ['ps_video_in', 'duplicate_accel',
+     [['rgb2gray_accel', 'filter2d_accel', 'colorthresholding_accel',
+     'gray2rgb_accel'], [1]], 'pr/add_accel', 'ps_video_out']),
 ]
 
 
@@ -58,14 +53,14 @@ def generate_valid_permutations(dfx_ip: list, cpipe) -> list:
 
     """Generate all valid permute, not repeated DFX region"""
     valid_permute = []
-    for idx in range(len(all_permute)):
+    for i, v in enumerate(all_permute):
         duplicate = False
-        for ii in range(len(all_permute[idx])):
-            if all_permute[idx][ii] in all_permute[idx][ii+1:]:
+        for ii, _ in enumerate(v):
+            if v[ii] in v[ii+1:]:
                 duplicate = True
                 break
         if not duplicate:
-            valid_permute.append(all_permute[idx])
+            valid_permute.append(v)
 
     return valid_permute
 
@@ -89,12 +84,64 @@ def generate_valid_apps() -> list:
 valid_apps = generate_valid_apps()
 
 
+def get_ip_object_from_overlay(ip, cpipe):
+    """Retrieve the actual IP driver object from the overlay"""
+
+    if isinstance(ip, int) or ip == '':
+        return ip
+    elif 'pr' in ip:
+        name = ip.split('/')[1]
+        for k, v in cpipe.c_dict.loaded.items():
+            if name in v['modtype'] and v['dfx'] and not v.get('used', False):
+                v['used'] = True
+                return getattr(cpipe, k)
+
+    return getattr(cpipe, ip)
+
+
+def convert_string_to_ip_object(app, cpipe) -> list:
+    """From list of IP name to use and generate object list with driver"""
+    obj = []
+    for ip_name in app:
+        if not isinstance(ip_name, list):
+            ip_obj = get_ip_object_from_overlay(ip_name, cpipe)
+            obj.append(ip_obj)
+        else:
+            branch = []
+            for ip_list in ip_name:
+                branch.append(convert_string_to_ip_object(ip_list, cpipe))
+            obj.append(branch)
+    return obj
+
+
+def get_dfx_regions_to_download(dfx, cpipe) -> list:
+    """Retrieve actual DFX region to download"""
+    dfx_regions = []
+    for ip in dfx:
+        name = ip.replace('bitand', 'bitwise')
+        for k, v in cpipe.c_dict.items():
+            if name.lower() in k.lower() and v['dfx']:
+                dfx_regions.append(k)
+                break
+    return dfx_regions
+
+
 @pytest.mark.parametrize('app', valid_apps)
 def test_app(app, create_composable):
-    #ol, cpipe = create_composable
-    #video = VideoStream(ol, VSource.OpenCV, VSink.DP, '../mountains.mp4')
-    #video.start()
-    #time.sleep(5)
-    #video.stop()
-    print(app)
-    assert True
+    """ This test will compose the apps and start a video stream
+
+    Each `app` contains the general name of dfx to download and pipeline
+    """
+    ol, cpipe = create_composable
+    dfx_regions = get_dfx_regions_to_download(app[0], cpipe)
+    cpipe.load(dfx_regions)
+    app_obj = convert_string_to_ip_object(app[1], cpipe)
+    cpipe.compose(app_obj)
+    video = VideoStream(ol, VSource.OpenCV, VSink.DP, '../mountains.mp4')
+    try:
+        video.start()
+        time.sleep(5)
+        assert video._video._started and video._video._running
+        video.stop()
+    except pytest.PytestUnhandledThreadExceptionWarning:
+        assert False, f"App {app} failed"
