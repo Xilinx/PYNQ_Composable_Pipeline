478ab0bf6c1778910c4c790945c16b2aaddc2ef8
Mario Ruiz <11815099+mariodruiz@users.noreply.github.com>
2022-11-25 11:18:38 +0000

    Enhance composable to support IP with multiple inputs and output simultaneously  (#122)
    * Add a new IP to the composable to test
    
    * Change file line ending
    
    * Add test when an IP has both two inputs and two outputs
    
    * Improve test
    
    * Add graphviz as package dependency
    
    * Install graphviz dependency with apt
    
    * Refactor code to support IP with multiple inputs and outputs at the same time
    
    * Fix exception message
    
    * Add dfx f9 function to the list of IP that can be used
    
    * Add test to verify unloaded IP exception
    
    * Include mock class to test .start()

--- tests/test_composable.py
+++ tests/test_composable.py
@@ -38,22 +38,38 @@ with open('tests/files/composable_test.hwh') as f:
     hier_desc['device'] = MockDevice(f.name)
 
 
+class DefaultIPStart(pynq.DefaultIP):
+    def start(self):
+        pass
+
+
 class MockOverlayComposable(MockOverlay):
     def __init__(self, desc):
         self.sw = switch.StreamSwitch(desc)
         self.sw._fullpath = desc['fullpath']
         self.hierarchies = {'composable': {'ip': None}}
         self.ip_dict = dict()
+        self.f9 = DefaultIPStart({
+                                  'phys_addr': 12288,
+                                  'addr_range': 1024,
+                                  'fullpath': 'composable/pr/f9',
+                                  'registers': {}
+                                 })
+        self.f8 = virtual.DFXRegion(self, 'composable/pr/f8')
 
     def __getattr__(self, name):
         # TODO check why composable is included two times
         if 'composable/axis_switch' in name:
             return self.sw
+        elif 'f9' in name:
+            return self.f9
+        elif 'f8' in name:
+            return self.f8
         return super()
 
 
 desc = {"parameters":
-        {"C_BASEADDR": "0x0", "C_HIGHADDR": "0xFFFF", "NUM_MI": 13},
+        {"C_BASEADDR": "0x0", "C_HIGHADDR": "0xFFFF", "NUM_MI": 15},
         "phys_addr": 0x0, "addr_range": 0xFFFF,
         "fullpath": "composable/axis_switch"}
 
@@ -111,44 +127,64 @@ def test_composable_exception_connection(hierarchy, pipeline):
     cpipe, _ = hierarchy
     with pytest.raises(SystemError) as excinfo:
         cpipe.compose(eval(pipeline))
-    assert "Not all IPs within the pipeline were assigned" in str(excinfo.value)
+    assert "pipeline was not connected correctly." in str(excinfo.value)
+
 
 pipelines = [
     ("[cpipe.f0, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4, cpipe.f5, cpipe.f6,\
        cpipe.f7]",
      {'0': 2, '64': 1 << 31, '68': 0, '72': 1, '76': 2, '80': 3, '84': 4,
       '88': 5, '92': 6, '96': 1 << 31, '100': 1 << 31, '104': 1 << 31,
-      '108': 1 << 31, '112': 1 << 31}),
+      '108': 1 << 31, '112': 1 << 31, '116': 1 << 31, '120': 1 << 31}),
 
     ("[cpipe.f0, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4]",
      {'0': 2, '64': 1 << 31, '68': 0, '72': 1, '76': 2, '80': 3,
       '84': 1 << 31, '88': 1 << 31, '92': 1 << 31, '96': 1 << 31,
-      '100': 1 << 31, '104': 1 << 31, '108': 1 << 31, '112': 1 << 31}),
+      '100': 1 << 31, '104': 1 << 31, '108': 1 << 31, '112': 1 << 31,
+      '116': 1 << 31, '120': 1 << 31}),
 
     ("[cpipe.f0, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4, cpipe.f6, cpipe.f7]",
      {'0': 2, '64': 1 << 31, '68': 0, '72': 1, '76': 2, '80': 3,
       '84': 1 << 31, '88': 4, '92': 6, '96': 1 << 31, '100': 1 << 31,
-      '104': 1 << 31, '108': 1 << 31, '112': 1 << 31}),
+      '104': 1 << 31, '108': 1 << 31, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31}),
 
     ("[cpipe.f0, cpipe.f5, cpipe.f2, cpipe.f3, cpipe.f4, cpipe.f6, cpipe.f7]",
      {'0': 2, '64': 1 << 31, '68': 1 << 31, '72': 5, '76': 2, '80': 3,
       '84': 0, '88': 4, '92': 6, '96': 1 << 31, '100': 1 << 31,
-      '104': 1 << 31, '108': 1 << 31, '112': 1 << 31}),
+      '104': 1 << 31, '108': 1 << 31, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31}),
 
     ("[cpipe.f1, cpipe.fork, [[cpipe.f3], [1]], cpipe.join, cpipe.f2]",
      {'0': 2, '64': 1 << 31, '68': 1 << 31, '72': 8, '76': 9, '80': 1 << 31,
       '84': 1 << 31, '88': 1 << 31, '92': 1 << 31, '96': 3, '100': 10,
-      '104': 1, '108': 1 << 31, '112': 1 << 31}),
+      '104': 1, '108': 1 << 31, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31}),
 
     ("[cpipe.f1, cpipe.fork, [[cpipe.f7], [1]], cpipe.join, cpipe.f2]",
      {'0': 2, '64': 1 << 31, '68': 1 << 31, '72': 8, '76': 1 << 31,
       '80': 1 << 31, '84': 1 << 31, '88': 1 << 31, '92': 9, '96': 7, '100': 10,
-      '104': 1, '108': 1 << 31, '112': 1 << 31}),
+      '104': 1, '108': 1 << 31, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31}),
 
     ("[cpipe.f7, cpipe.fork, [[1], [1]], cpipe.join, cpipe.f0, cpipe.f2]",
      {'0': 2, '64': 8, '68': 1 << 31, '72': 0, '76': 1 << 31, '80': 1 << 31,
       '84': 1 << 31, '88': 1 << 31, '92': 1 << 31, '96': 9, '100': 10,
-      '104': 7, '108': 1 << 31, '112': 1 << 31})
+      '104': 7, '108': 1 << 31, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31}),
+
+    ("[cpipe.f7, cpipe.fork, [[cpipe.dual, cpipe.f0], [cpipe.dual]], \
+        cpipe.join, cpipe.f3, cpipe.f2]",
+     {'0': 2, '64': 13, '68': 1 << 31, '72': 3, '76': 8, '80': 1 << 31,
+      '84': 1 << 31, '88': 1 << 31, '92': 1 << 31, '96': 0, '100': 14,
+      '104': 7, '108': 1 << 31, '112': 1 << 31, '116': 9,
+      '120': 10}),
+
+    ("[cpipe.f7, cpipe.fork, [[cpipe.f9], [1]], cpipe.join, cpipe.f0]",
+     {'0': 2, '64': 8, '68': 1 << 31, '72': 1 << 31, '76': 1 << 31,
+      '80': 1 << 31, '84': 1 << 31, '88': 1 << 31, '92': 1 << 31, '96': 11,
+      '100': 10, '104': 7, '108': 9, '112': 1 << 31, '116': 1 << 31,
+      '120': 1 << 31})
 ]
 
 
@@ -164,15 +200,26 @@ def test_composable_exception_multiple_bad_slots(hierarchy):
     with pytest.raises(SystemError) as excinfo:
         cpipe.compose([cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4, cpipe.f5,
                        cpipe.f6, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4,
-                       cpipe.f5, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4])
+                       cpipe.f5, cpipe.f1, cpipe.f2, cpipe.f3, cpipe.f4,
+                       cpipe.f1])
     assert "Number of slots in the list is bigger than" in str(excinfo.value)
+
+
+def test_composable_exception_compose_unloaded_ip(hierarchy):
+    cpipe, _ = hierarchy
+    with pytest.raises(AttributeError) as excinfo:
+        cpipe.compose([cpipe.source_data, cpipe.pr.f8, cpipe.sink_data])
+    assert "is not loaded, load IP before composing" in str(excinfo.value)
 
 
 @pytest.mark.parametrize('pipeline', pipelines)
 def test_composable_pipeline(hierarchy, pipeline):
     cpipe, ipdevice = hierarchy
     pipe = eval(pipeline[0])
+    cpipe._graph_debug = True
     cpipe.compose(pipe)
+    name = f'pipeline{pipelines.index(pipeline)}'
+    cpipe.graph.render(format='png', outfile=f'tests/graph_output/{name}.png')
     assert ipdevice.ip.memory == pipeline[1]
 
 
