76826a54c4e48a241e8da55c152186817da51b50
Mario Ruiz <mruiznog@amd.com>
2023-01-27 16:38:33 +0000

    Pass regions for future use

--- pynq_composable/runtime_tests/test_relocatable_apps.py
+++ pynq_composable/runtime_tests/test_relocatable_apps.py
@@ -148,7 +148,7 @@ def generate_valid_apps() -> list:
 valid_apps = generate_valid_apps()
 
 
-def get_ip_object_from_overlay(ip, cpipe):
+def get_ip_object_from_overlay(ip, cpipe, regions):
     """Retrieve the actual IP driver object from the overlay"""
 
     if isinstance(ip, int) or ip == '':
@@ -163,17 +163,17 @@ def get_ip_object_from_overlay(ip, cpipe):
     return getattr(cpipe, ip)
 
 
-def convert_string_to_ip_object(app, cpipe) -> list:
+def convert_string_to_ip_object(app, cpipe, regions) -> list:
     """From list of IP name to use and generate object list with driver"""
     obj = []
     for ip_name in app:
         if not isinstance(ip_name, list):
-            ip_obj = get_ip_object_from_overlay(ip_name, cpipe)
+            ip_obj = get_ip_object_from_overlay(ip_name, cpipe, regions)
             obj.append(ip_obj)
         else:
             branch = []
             for ip_list in ip_name:
-                branch.append(convert_string_to_ip_object(ip_list, cpipe))
+                branch.append(convert_string_to_ip_object(ip_list, cpipe, regions))
             obj.append(branch)
     return obj
 
@@ -208,7 +208,7 @@ def test_app(app, create_composable):
         pipeline[0] = 'hdmi_sink_in'
         pipeline[-1] = 'hdmi_sink_out'
 
-    app_obj = convert_string_to_ip_object(pipeline, cpipe)
+    app_obj = convert_string_to_ip_object(pipeline, cpipe, app[0])
     cpipe._graph_debug = True
     cpipe.compose(app_obj)
     file = '../mountains.mp4' if pytest.videofile else 0
